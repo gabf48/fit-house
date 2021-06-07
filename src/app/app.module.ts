fb4969181a940499f56c981cd259fffec748ef7a
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-06-07 22:46:07 +0300

    start working at Tarife

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { EchipaModule } from './echipa/echipa.module';
-import { EchipaComponent } from './echipa/echipa.component';
 import { CalculatorModule } from './calculator/calculator.module';
-import { LoggingService } from './logging.service';
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
-import { CalculatorComponent } from './calculator/calculator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +22,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     CoreModule,
     CalculatorModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
  
       bootstrap: [AppComponent],
--- src/app/tarife/tarife.module.ts
+++ src/app/tarife/tarife.module.ts
@@ -1,12 +1,13 @@
 import { TarifeComponent } from './tarife.component';
 import { NgModule } from "@angular/core";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [TarifeComponent],
     imports: [
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild([
             { path: '', component: TarifeComponent },
         ])
