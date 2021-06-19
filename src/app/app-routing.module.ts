7784dd95c8be436f0db9004bb295b15cd00b146d
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-06-19 22:23:41 +0300

    clear code and add more features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
-import { TarifeModule } from './tarife/tarife.module';
-import { HomefitModule } from './homefit/homefit.module';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-
-  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module')
-  .then(m => m.RecipesModule) },
 
   { path: 'shopping-list', 
   loadChildren: () => import('./shopping-list/shopping-list.module')
