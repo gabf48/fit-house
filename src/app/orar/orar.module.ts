a58fbc1b5e0b0430eeaa7ba4b2291ce260011d84
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-06-27 22:38:51 +0300

    update orar page

--- src/app/orar/orar.module.ts
+++ src/app/orar/orar.module.ts
@@ -2,11 +2,13 @@ import { OrarComponent } from './orar.component';
 import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
     declarations: [OrarComponent],
     imports: [  FormsModule,
+        FontAwesomeModule,
         RouterModule.forChild([
             { path: '', component: OrarComponent },
         ])]
