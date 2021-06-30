9afe1a391be4e8dcb8397ed278474d7e0e2b5d1f
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-06-30 14:36:50 +0300

    retusuri finale

--- src/app/cont/date-personale/detalii-users.ts
+++ src/app/cont/date-personale/detalii-users.ts
@@ -1,18 +1,17 @@
 import { ECHIPA } from './../../echipa/echipa-detalii';
-import { Echipa } from './../../echipa/echipa';
 import { Users } from './users';
 
 export const USERS: Users[] = [
-  { nume: 'Rus', prenume: 'Andreas', sex: 'masculin' , an_nastere: 1993, adresa: 'Cluj', poza:'', email: 'gabf48@gmail.com', antrenor: ECHIPA[6]},
-  { nume: 'Pop', prenume: 'Andrei', sex: 'masculin' , an_nastere: 1985, adresa: 'Alba', poza:'', email: 'test@test.com', antrenor: ECHIPA[1] },
-  { nume: 'Popa', prenume: 'Mircea', sex: 'masculin' , an_nastere: 1972, adresa: 'Sibiu', poza:'', email: 'antrenor@fithouse.com', antrenor: ECHIPA[2]},
-  { nume: 'Radu', prenume: 'Dimitru', sex: 'masculin' , an_nastere: 1988, adresa: 'Mures', poza:'', email: 'radu88@gmail.com', antrenor: ECHIPA[3]},
-  { nume: 'Mironescu', prenume: 'Mironescu', sex: 'feminin' , an_nastere: 1983, adresa: 'Oradea', poza:'', email: 'gabriela_m@gmail.com', antrenor: ECHIPA[9]},
-  { nume: 'Adriana', prenume: 'Bucur', sex: 'feminin' , an_nastere: 1984, adresa: 'Dej', poza:'', email: 'adriana@gmail.com', antrenor: ECHIPA[8]},
-  { nume: 'Filipescu', prenume: 'Manuel', sex: 'masculin' , an_nastere: 1988, adresa: 'Teleorman', poza:'', email: 'manu_f@gmail.com', antrenor: ECHIPA[5]},
-  { nume: 'Rusu', prenume: 'Alex', sex: 'masculin' , an_nastere: 1991, adresa: 'Bucuresti', poza:'', email: 'alex91@gmail.com', antrenor: ECHIPA[7]},
-  { nume: 'Nicu', prenume: 'Cecilia', sex: 'feminin' , an_nastere: 1996, adresa: 'Alba', poza:'', email: 'cecilia_nn@gmail.com', antrenor: ECHIPA[6]},
-  { nume: 'Roman', prenume: 'Danut', sex: 'masculin' , an_nastere: 1995, adresa: 'Oradea', poza:'', email: 'danut95@gmail.com', antrenor: ECHIPA[4]},
-  { nume: 'Popa', prenume: 'Darius', sex: 'masculin' , an_nastere: 1996, adresa: 'Cluj', poza:'', email: 'dariuss@gmail.com', antrenor: ECHIPA[8]},
-  { nume: 'Moldovan', prenume: 'Diana', sex: 'feminin' , an_nastere: 1994, adresa: 'Bistrita', poza:'', email: 'dianna@gmail.com', antrenor: ECHIPA[1]}
+  { nume: 'Rus', prenume: 'Andreas', sex: 'masculin' , an_nastere: 1994, adresa: 'Alba', poza:'../assets/profile/rus_andreas.jpg', email: 'gabf48@gmail.com', antrenor: ECHIPA[6]},
+  { nume: 'Filip', prenume: 'Gabriel', sex: 'masculin' , an_nastere: 1993, adresa: 'Cluj', poza:'../assets/profile/filip_gabriel.jpg', email: 'test@test.com', antrenor: ECHIPA[1] },
+  { nume: 'Popa', prenume: 'Mircea', sex: 'masculin' , an_nastere: 1972, adresa: 'Sibiu', poza:'../assets/profile/popa_mircea.jpg', email: 'antrenor@fithouse.com', antrenor: ECHIPA[2]},
+  { nume: 'Radu', prenume: 'Dimitru', sex: 'masculin' , an_nastere: 1988, adresa: 'Mures', poza:'../assets/profile/radu_dimitriu.jpg', email: 'radu88@gmail.com', antrenor: ECHIPA[3]},
+  { nume: 'Mironescu', prenume: 'Gabriela', sex: 'feminin' , an_nastere: 1983, adresa: 'Oradea', poza:'../assets/profile/gabriela_mironescu.jpg', email: 'gabriela_m@gmail.com', antrenor: ECHIPA[9]},
+  { nume: 'Adriana', prenume: 'Bucur', sex: 'feminin' , an_nastere: 1984, adresa: 'Dej', poza:'../assets/profile/adriana_bucur.jpg', email: 'adriana@gmail.com', antrenor: ECHIPA[8]},
+  { nume: 'Filipescu', prenume: 'Manuel', sex: 'masculin' , an_nastere: 1988, adresa: 'Teleorman', poza:'../assets/profile/filipescu_manuel.jpg', email: 'manu_f@gmail.com', antrenor: ECHIPA[5]},
+  { nume: 'Rusu', prenume: 'Alex', sex: 'masculin' , an_nastere: 1991, adresa: 'Bucuresti', poza:'../assets/profile/rusu_alex.jpg', email: 'alex91@gmail.com', antrenor: ECHIPA[7]},
+  { nume: 'Nicu', prenume: 'Cecilia', sex: 'feminin' , an_nastere: 1996, adresa: 'Alba', poza:'../assets/profile/nicu_cecilia.jpg', email: 'cecilia_nn@gmail.com', antrenor: ECHIPA[6]},
+  { nume: 'Roman', prenume: 'Danut', sex: 'masculin' , an_nastere: 1995, adresa: 'Oradea', poza:'../assets/profile/roman_danut.jpg', email: 'danut95@gmail.com', antrenor: ECHIPA[4]},
+  { nume: 'Popa', prenume: 'Darius', sex: 'masculin' , an_nastere: 1996, adresa: 'Cluj', poza:'../assets/profile/popa_darius.jpg', email: 'dariuss@gmail.com', antrenor: ECHIPA[8]},
+  { nume: 'Moldovan', prenume: 'Diana', sex: 'feminin' , an_nastere: 1994, adresa: 'Bistrita', poza:'../assets/profile/moldovan_diana.jpg', email: 'dianna@gmail.com', antrenor: ECHIPA[1]}
 ];
