5012e06e8c39d7837dfdd1f8a7f3d9400dc8126e
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2021-12-04 19:54:17 +0200

    language styling

--- src/app/tarife/detalii-abonamente.ts
+++ src/app/tarife/detalii-abonamente.ts
@@ -2,11 +2,11 @@ import { Abonamente } from './abonamente';
 
 export const ABONAMENTE: Abonamente[] = [
   { id: 1, name: 'Prima ședință', pret: 'Gratis', descriere: 'Chiar dacă vrei să te antrenezi cu antrenor sau doar să faci exerciții în sală, prima ședință este gatuită!' },
-  { id: 2, name: 'Abonament lunar', pret: '100 RON', descriere: 'Ai acces la sală, la orice oră și în orce zi, în care găsești sala deschisă. Poți intra de câte ori vrei tu pe zi și sta cât îți face plăcere.'},
+  { id: 2, name: 'Abonament lunar', pret: '100 RON', descriere: 'Ai acces la sală, la orice oră și în orice zi, în care găsești sala deschisă. Poți intra de câte ori vrei tu pe zi și sta cât îți face plăcere.'},
   { id: 3, name: 'Abonament de familie', pret: '180 RON', descriere: 'Dacă vii cu partenera/partenerul ai o mică reducere. Nu ne trebuie niciun act doveditor. Doar covinge pe cineva să-ți spună în fața tuturor că te iubește.'},
-  { id: 4, name: 'Trainer personal 12 ședinte', pret: '300 RON', descriere: 'Dacă vrei să iei lucrurile puțin mai în serios, alege un antrenor personal. Detalii despre antrenorii noștri găsești și pe pagina Echipă. Nu vei regreta și vei avea rezultatele dorite în cel mai scurt timp posibil. De asemenea să nu uiți că antrenorii noștri sunt acreditați și sunt plini de experiență. Acest lucru îl vei putea vedea chiar pe corpurile lor.'},
-  { id: 5, name: 'Trainer personal 18 ședinte', pret: '330 RON', descriere: 'Ședințele suplimentare se vor desfăsura în zilele în care puteți găsi o oră favorabilă împreună cu antrenorul. Arată că îți pasă de corpul tău și dă-i ce-i mai de preț!' },
-  { id: 6, name: 'Saună ședinta', pret: '10 RON', descriere: 'Încearcă sauna noastră de ori câte ori ai chef și bucură-te de beneficiile ei. O ședință înseamnă maxim o oră. Acest timp este în beneficiul tuturor.' },
+  { id: 4, name: 'Trainer personal 12 ședințe', pret: '300 RON', descriere: 'Dacă vrei să iei lucrurile puțin mai în serios, alege un antrenor personal. Detalii despre antrenorii noștri găsești și pe pagina Echipă. Nu vei regreta și vei avea rezultatele dorite în cel mai scurt timp posibil. De asemenea să nu uiți că antrenorii noștri sunt acreditați și sunt plini de experiență. Acest lucru îl vei putea vedea chiar pe corpurile lor.'},
+  { id: 5, name: 'Trainer personal 18 ședințe', pret: '330 RON', descriere: 'Ședințele suplimentare se vor desfășura în zilele în care puteți găsi o oră favorabilă împreună cu antrenorul. Arată că îți pasă de corpul tău și dă-i ce-i mai de preț!' },
+  { id: 6, name: 'Saună ședință', pret: '10 RON', descriere: 'Încearcă sauna noastră de ori câte ori ai chef și bucură-te de beneficiile ei. O ședință înseamnă maxim o oră. Acest timp este în beneficiul tuturor.' },
   { id: 7, name: 'Saună abonament', pret: '150 RON', descriere: 'Bucură-te zilnic de saună și crează-ți un obicei bun de a nu lipsi de aici. Nu o să regreți.' }
 
 ];
